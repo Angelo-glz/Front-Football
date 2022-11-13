5004940c5706defe5ffb3ff650cf1d64f2f14172
Angelo Gonzalez <angelo.uvm0018@gmail.com>
2022-11-13 19:10:49 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEditPlayerComponent } from './add-edit-player/add-edit-player.component';
+
+
+
+import { EditTeamComponent } from './edit-team/edit-team.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      AddEditPlayerComponent,
+      EditTeamComponent,
+      TeamListComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/player.ts
@@ -0,0 +1,8 @@
+export interface Player{
+  id?:number,
+  name:string,
+  position:string,
+  status:string,
+  number?:number,
+  rating:string
+}
