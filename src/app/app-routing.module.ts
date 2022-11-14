63366e9c744a1031fb5658bcb605beb0a93f3bde
Angelo Gonzalez <angelo.uvm0018@gmail.com>
2022-11-13 21:00:32 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditPlayerComponent } from './add-edit-player/add-edit-player.component';
+import { EditTeamComponent } from './edit-team/edit-team.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'playersList', pathMatch: 'full' },
+  { path: 'playersList', component: TeamListComponent },
+  { path: 'addPlayer', component: AddEditPlayerComponent },
+  { path: 'editPlayer/:id', component: AddEditPlayerComponent },
+  { path: 'editTeam', component: EditTeamComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
